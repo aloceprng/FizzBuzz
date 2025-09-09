9c558f71ed412991e197b05500f4f05836d0e211
aloceprng <alicepeng72@gmail.com>
2025-09-09 12:48:22 -0400

    reduce

--- test/lab1/Reduce.java
+++ test/lab1/Reduce.java
@@ -1,5 +1,20 @@
 package lab1;
 
 public class Reduce {
-
+    static void main() {
+        System.out.println(reduce(100));
+    }
+    public static int reduce(int n) {
+        int total = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            }
+            else {
+                n -= 1;
+            }
+            total += 1;
+        }
+        return total;
+    }
 }
